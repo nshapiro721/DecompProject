8f018e36b7bb1f59f5d752cadb87a01fd27962a0
Noa Shapiro-Tamir <noashapiro-tamir@Noas-MacBook-Air.local>
2023-07-28 22:05:52 -0400

    Lots of work on 7/28

--- exec/decomp_model.R
+++ exec/decomp_model.R
@@ -29,6 +29,8 @@ df <- df %>%
 ggplot(df, aes(x = days_to_collection, y = PercMassRemaining)) + geom_point()
 boxplot(df$PercMassRemaining ~ df$days_to_collection, outlier_tagging = TRUE)
 
+find_outliers_df <- df%>%
+  filter(SLC == "Mo3.Morella")
 
 # direct estimate of alpha for each measurement point
 # this function lives in "exec/util.R" and is read in via the source() call above
@@ -96,24 +98,28 @@ lines(
   col = "blue"
 )
 
-#trying to do self-starting model... IT WORKS NOW???
-fit <- nls(PercMassRemaining ~ SSasymp(days_to_collection, yf, y0, log_alpha), data = df %>% filter(treatment == "Morella"))
+#trying to do self-starting model... IT WORKS NOW!
+fit_morella <- nls(PercMassRemaining ~ SSasymp(days_to_collection, yf, y0, log_alpha), data = df %>% filter(treatment == "Morella"))
 
-fit
+fit_morella
 
+#Plotting decay curve of non-iterated self-starting model
+    #this first one doesn't work yet
 grid_morella <- data.frame(x = seq(0, 345, length = 346))
-grid %>% add_predictions(fit)
+grid_morella %>% add_predictions(fit_morella)
 
-plot(df$PercMassRemaining ~ df$days_to_collection)
-curve(predict(fit, grid_morella))
+  plot(df$PercMassRemaining ~ df$days_to_collection)
+curve(predict(fit_morella, grid_morella))
 
+  #this one should work
 df%>% filter(treatment == "Morella") %>% 
-  add_predictions(fit) %>%
+  add_predictions(fit_morella) %>%
   ggplot(aes(x = days_to_collection, y = PercMassRemaining, group = treatment)) + 
   geom_point() + 
-  geom_line(aes(x=days_to_collection, y=pred))
+  geom_line(aes(x=days_to_collection, y=pred)) + 
+  ggtitle("Decay Curve: Morella Treatment")
 
-#Iterating noa_model for all treatments 
+#Iterating noa_model for all treatments - BAD ALPHAS
 treatment_alphas <- df %>% 
   nest(-treatment) %>%
   mutate(
@@ -128,17 +134,16 @@ treatment_alphas_tbl <- treatment_alphas %>%
 
 treatment_alphas_tbl
 
-df%>%
-  add_predictions(treatment_alphas)
-ggplot(df, aes(x = days_to_collection, y = PercMassRemaining, group = treatment))
 
-deviance(noa_model)
+ggplot(treatment_alphas_ss_tbl, aes(x = treatment, y = log_alpha)) + geom_bar(stat = "identity", width = 0.5) + 
+  geom_errorbar(aes(ymin = log_alpha-std.error, ymax = log_alpha+std.error), width = 0.2)
 
-#Iterating noa_model for all Site/Treatment combos
+
+#Iterating noa_model for all Site/Treatment combos - BAD ALPHAS
 SLC_alphas <- df %>% 
   nest(-SLC) %>%
   mutate(
-    fit = map(data, ~nls(PercMassRemaining ~ 1* exp(-a * days_to_collection), data = ., start = list(a = 0.09))),
+    fit = map(data, ~nls(PercMassRemaining ~ 1* exp(-a * days_to_collection), data = ., start = list(a = 0.01))),
     tidied = map(fit, tidy),
     Augmented = map(fit, augment),)
 
@@ -150,62 +155,83 @@ SLC_alphas_tbl <-
 
 SLC_alphas_tbl
 
-
-#trying to get the self-starter to iterate. Not working yet.
+#trying to get the self-starter to iterate. works for treatments, not for SLC
 treatment_alphas_ss <- df %>%
   nest(-treatment) %>%
   mutate(
-    fit = map(data, ~nls(PercMassRemaining ~ SSasymp(days_to_collection, yf, y0, log_alpha), control = list(maxiter = 500), data = . )),
+    fit = map(data, ~nls(PercMassRemaining ~ SSasymp(fake_days, yf, y0, log_alpha), control = list(maxiter = 500), data = . )),
               tidied = map(fit, tidy),
               Augmented = map(fit, augment),)
 
 treatment_alphas_ss_tbl <- treatment_alphas_ss %>% 
   unnest(tidied) %>% 
-  select(treatment, term, estimate, std.error, p.value, statistic) %>% 
-  spread(term, estimate)
-treatment_alphas_ss_tbl
-
-df%>%
-ggplot(aes(x = days_to_collection, y = PercMassRemaining, group = treatment)) + 
-  geom_point() + 
-  geom_line(aes(x=days_to_collection, y=treatment_alphas_ss$fit))
+  select(treatment, term, estimate, std.error) %>% 
+  spread(term, estimate) %>%
+  mutate(k = exp(log_alpha))%>%
+  filter (!is.na(k))
+treatment_alphas_ss_tbl 
 
 
-SLC_alphas_ss <- df %>%
-  nest(-SLC) %>%
+
+#trying to iterate model for SLC, but it only works for site now?
+site_alphas_ss <- df %>%
+  nest(-site) %>%
   mutate(
     fit = map(data, ~nls(PercMassRemaining ~ SSasymp(days_to_collection, yf, y0, log_alpha), control = list(maxiter = 500), data = . )),
               tidied = map(fit, tidy),
               Augmented = map(fit, augment),)
 
-SLC_alphas_ss_tbl <- SLC_alphas_ss %>% 
+site_alphas_ss_tbl <- site_alphas_ss %>% 
   unnest(tidied) %>% 
-  select(SLC, term, estimate) %>% 
+  select(site, term, estimate) %>% 
   spread(term, estimate)
 
-SLC_alphas_ss_tbl
-#joining alpha values to df so that we can plot against all variables
-df <- merge(df, treatment_alphas_tbl, by = "treatment")
-df$treatment_alpha = df$a
-df<- mutate(df, a = NULL)
+site_alphas_ss_tbl
+
+#joining k values to df so that we can plot against all variables
+df <- merge(df, treatment_alphas_ss_tbl, by = "treatment")
+df$trtmt_k = df$k
+df$trtmt_log_alph = df$
+df<- mutate(df, k = NULL)
 
 df <- merge(df, SLC_alphas_tbl, by = "SLC")
 df$SLC_alpha = df$a
 df<- mutate(df, a = NULL)
   
-#graphs of alpha values
+#graphs of k values
 
-treatment_plot <- ggplot(data = df, aes(x = treatment, y = treatment_alpha)) + theme(axis.text.x = element_text(angle = 90)) + geom_point()
+treatment_plot <- ggplot(data = df, aes(x = treatment, y = k)) + theme(axis.text.x = element_text(angle = 90)) + geom_point()
 treatment_plot
+
        
 SLC_plot <- ggplot(data = df, aes(x = SLC, y = SLC_alpha)) + theme(axis.text.x = element_text(angle = 90)) + geom_point(aes(colour = treatment))
 
 SLC_plot
 
 #plotting the curves themselves
-qplot(df$days_to_collection, df$PercMassRemaining, data = augment(fit)) + geom_line(aes(y = .fitted))
+  
+  #the following uses the code from Douglas watson for graphing multiple curves from an interated model, but because our data is set up in this long form with many values for each treatment at each time point, it's not making any sense to look at. I think what we need here is to be seeing the lines for each SLC, not each treatment? because that's the only thing that can actually be tracked with a line over time.
+augmented <- treatment_alphas_ss %>% 
+  unnest(Augmented)
 
-plot(df$days_to_collection,df$PercMassRemaining)
-curve(predict(treatment_alphas_tbl, data.frame(x)))
+qplot(df$days_to_collection, df$PercMassRemaining, data = augmented, geom = 'point', colour = treatment) 
++ 
+  geom_line(aes(y = .fitted))
+  
+  #brute forcing treatment curves, a la Alex's advice, using functions written in util.R
+head(mor_eq(fake_days))
 
-      
+fake_days <- seq(1:345)
+
+plot(x = fake_days, y = mor_eq(fake_days), main = "Morella brute force")
+
+plot(x = fake_days, y = pin_eq(fake_days), main = "Pine brute force")
+
+plot(x = fake_days, y = phrag_eq(fake_days), main = "Phrag brute force")
+
+df%>% 
+  add_predictions(treatment_alphas_ss)%>%
+  ggplot(aes(x = days_to_collection, y = PercMassRemaining, group = treatment)) + 
+  geom_point() + 
+  geom_line(aes(x=days_to_collection, y=pred))
+#this tries to create graphs from not a single model but the iterated versions, but it doesn't know how to predict from a model with so many nested layers, and I don't blame it. Help!
