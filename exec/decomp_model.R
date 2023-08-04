b22508a8a4ebd1547aa6aa37b36a41b517c6319f
Noa <nshapiro@oberlin.edu>
2023-08-04 19:44:03 -0400

    A LITTLE CLOSER

--- exec/decomp_model.R
+++ exec/decomp_model.R
@@ -73,15 +73,27 @@ lines(
   col = "blue"
 )
 
-# Noa makes the same model but with percents. Has a lower residual sum of squares so kept it. Want to double check the
+# Noa makes the same model but with percents, and makes one for each of the three treatments, since we'll need them to graph the curves later. Has a lower residual sum of squares so kept it.
 
-noa_model <- nls(
+morella_model <- nls(
   PercMassRemaining ~ 1 * exp(-a * years_to_collection),
   data = df %>% filter(treatment == "Morella"),
   start = list(a = 0.02)
                 )
 
-noa_model
+morella_model
+
+phrag_model <- nls(
+  PercMassRemaining ~ 1 * exp(-a * years_to_collection),
+  data = df %>% filter(treatment == "Phragmites"),
+  start = list(a = 0.02)
+)
+
+pine_model <- nls(
+  PercMassRemaining ~ 1 * exp(-a * years_to_collection),
+  data = df %>% filter(treatment == "Pine"),
+  start = list(a = 0.02)
+)
 
 
 # Testing Noa model - trying to do here what Riley did with Cool model. Not sure if it actually shows us new info or not.
@@ -101,7 +113,7 @@ lines(
   col = "blue"
 )
 
-# Iterating noa_model for all treatments
+# Iterating noa_model (now morella_model) for all treatments
 treatment_alphas <- df %>%
   nest(-treatment) %>%
   mutate(
@@ -128,7 +140,7 @@ ggplot(treatment_alphas_tbl, aes(x = treatment, y = a)) +
   geom_point(aes(col = treatment), size = 2) +
   geom_errorbar(aes(ymin = a - std.error, ymax = a + std.error, col = treatment), width = 0.1)
 
-# Iterating noa_model for all Site/Treatment combos
+# Iterating noa_model (now morella_model) for all Site/Treatment combos
 SLC_alphas <- df %>%
   nest(-SLC) %>%
   mutate(
@@ -200,19 +212,12 @@ summary(SLC_anova)
 TukeyHSD(SLC_anova, "treatment")
 
 # plotting the curves themselves
+
+  #first, some tests...
 four_years <- as.data.frame(seq(1, 4, 0.02)) %>%
   rename(four_years_continuous = seq)
 
 ten_years <- as.data.frame(seq(1, 10, 0.02))
-
-df %>% 
-  add_predictions(noa_model_tester) %>%
-  ggplot(aes(x = four_years, y = PercMassRemaining)) +
-  geom_point(col = "dark gray") +
-  geom_smooth(aes(group = treatment, col = treatment, y = pred, fill = treatment), alpha = 0.3) +
-ggtitle("Decay Curves Predicted by Modeled Decay Coefficients") 
-
-
 noa_model_tester <- nls(
   PercMassRemaining ~ 1 * exp(-a * years_to_collection),
   data = df %>% filter(treatment == "Morella"),
@@ -226,37 +231,52 @@ t_to_eyeball_fit <- 1:1000
 perc_mass_to_eyeball_fit_four <- four_years %>% 
   rename("year" = "seq(1, 4, 0.02)") %>%
   mutate (pmr = unlist(get_mass_pct_remaining_at_t(
-                  alpha = coef(noa_model_tester)[["a"]],
-                  t = four_years)))
+    alpha = coef(noa_model_tester)[["a"]],
+    t = four_years)))
 
 perc_mass_to_eyeball_fit_ten <- ten_years %>% 
   rename("year" = "seq(1, 10, 0.02)") %>%
   mutate (pmr = unlist(get_mass_pct_remaining_at_t(
     alpha = coef(noa_model_tester)[["a"]],
     t = ten_years)))
-                                          
+
 
 perc_mass_to_eyeball_fit
 
 ggplot(data = perc_mass_to_eyeball_fit_ten, aes(x = year, y = pmr)) +
-  geom_point()
+  geom_point()+
+  geom_point(data = df %>%
+               filter(treatment == "Morella") %>%
+               add_predictions(model = morella_model),
+             aes(x = years_to_collection, y = PercMassRemaining))
 
-plot(perc_mass_to_eyeball_fit$pmr ~ perc_mass_to_eyeball_fit$year)
+  #now, the real thing
 
-plot(
-  x = df[which(df$treatment == "Morella"), "years_to_collection"],
-  y = df[which(df$treatment == "Morella"), "PercMassRemaining"]
-)
-lines(
-  x = t_to_eyeball_fit,
-  y = perc_mass_to_eyeball_fit,
-  t = "p",
-  col = "blue"
-)
+morella_preds <- df %>% filter(treatment == "Morella") %>%
+  add_predictions(morella_model) %>%
+  select(pred, tag)
 
-df %>%
- filter(treatment == "Morella") %>%
- add_predictions(model = noa_model) %>%
+phrag_preds <- df %>% filter(treatment == "Morella") %>%
+  add_predictions(phrag_model) %>%
+  select(pred, tag)
+
+pine_preds <- df %>% filter(treatment == "Morella") %>%
+  add_predictions(pine_model) %>%
+  select(pred, tag)
+
+preds <- rbind(morella_preds, phrag_preds, pine_preds)
+
+df_test <- merge(preds, df, by = "tag", na.rm = FALSE) %>%
+  filter(!is.na(class)) %>%
+  ggplot(aes(x = years_to_collection, y = PercMassRemaining)) +
+  geom_point(col = "dark gray") +
+  geom_smooth(aes(group = treatment, col = treatment, y = pred, fill = treatment), alpha = 0.3) +
+ggtitle("Deca Curves Predicted by Modeled Decay Coefficients") 
+
+df_test
+
+
+ %>%
    ggplot(aes(x = years_to_collection)) +
    geom_point(col = "dark gray", aes(y = pred)) +
 geom_line(aes(group = treatment, col = treatment, y = pred))+ 
@@ -273,7 +293,7 @@ geom_line(aes(group = treatment, col = treatment, y = pred))+
 
 add_predictions(df %>% 
                   filter(treatment == "Morella"), 
-                model = noa_model)
+                model = morella_model)
 
 #Bringing in soil data
 soils_decomp <- read.csv("data/Decomp_soils.csv")
